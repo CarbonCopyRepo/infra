d8d22db5b1cb76d7694060880723ab689b062e03
Tyler <>
2024-02-06 01:24:17 -0700

    .

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "carbon-copy_bucket_test123223" {
-  name                     = "Test"
+resource "google_storage_bucket" "test-bucket" {
+  name                     = "carbon_copy_bucket_test1"
   project                  = "carbonme"
   location                 = "US"
   force_destroy            = true
