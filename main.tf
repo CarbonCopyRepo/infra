1b20949d7972765e42f7e5f5a86770841f55625e
Tyler <>
2024-02-06 01:15:41 -0700

    added project

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "Test"
+  project                  = "carbonme"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
