27b8f45d49e77a692d86b5d33dc24c56bc23d33b
Tyler <>
2024-02-09 21:05:38 -0700

    added GCP bucket for Terraform State

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
-resource "google_storage_bucket" "test-bucket" {
-  name                     = "carbon_copy_bucket_test1"
-  project                  = "carbonme"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
+terraform {
+ backend "gcs" {
+   bucket  = "CarbonCopyTerraformStateBucket"
+   prefix  = "terraform/state"
+ }
 }
