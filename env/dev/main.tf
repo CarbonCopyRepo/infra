0372bc4e701a591ef62e1c125b6b6c76bceb67cc
Tyler <>
2024-03-02 19:24:29 -0700

    vpc setup

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,6 +1,14 @@
+/*
 terraform {
  backend "gcs" {
    bucket  = carbon_copy_storage_bucket
    prefix  = "terraform/state"
  }
+}
+*/
+
+resource "google_compute_network" "vpc_network" {
+  project = "CarbonCopy"
+  name = "vpc-network"
+  auto_create_subnetworks = true
 }
