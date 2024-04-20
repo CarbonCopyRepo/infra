6f8788e16bf02a4eb18f63b7d0a1e80096d04238
Tyler <>
2024-04-19 20:36:18 -0600

    Setup kubernetes on frontend. Will turn into a module

--- resources.tf
+++ resources.tf
@@ -1,17 +1,12 @@
-locals {
-  project_name = "carbonme"
-  zone         = "us-central1-a"
-}
-
 resource "google_compute_network" "vpc_network" {
-  project                 = local.project_name
-  name                    = "vpc-network"
+  project                 = var.project_id
+  name                    = "vpc-ntwork"
   auto_create_subnetworks = true
 }
 
 resource "google_compute_firewall" "ssh" {
   name          = "allow-ssh"
-  project       = local.project_name
+  project       = var.project_id
   direction     = "INGRESS"
   network       = google_compute_network.vpc_network.id
   priority      = 1000
@@ -24,17 +19,18 @@ resource "google_compute_firewall" "ssh" {
   }
 }
 
-resource "google_compute_firewall" "allow_https" {
-  name    = "allow-https"
-  project = local.project_name
-  network = google_compute_network.vpc_network.id
+resource "google_compute_firewall" "allow_http_https" {
+  name          = "allow-http-https"
+  project       = var.project_id
+  network       = google_compute_network.vpc_network.id
+  target_tags   = ["allow-http", "allow-https"]
+  source_ranges = ["0.0.0.0/0"]
+  priority      = 1000
 
   allow {
     protocol = "tcp"
-    ports    = ["443"]
+    ports    = ["443", "80"]
   }
-
-  source_ranges = ["0.0.0.0/0"]
 }
 
 resource "random_id" "bucket_prefix" {
@@ -42,11 +38,11 @@ resource "random_id" "bucket_prefix" {
 }
 
 resource "google_storage_bucket" "carbon_copy_storage_bucket" {
-  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  name          = "bucket-tfstate-415616"
   force_destroy = false
   location      = "US"
   storage_class = "STANDARD"
-  project       = local.project_name
+  project       = var.project_id
   versioning {
     enabled = true
   }
@@ -57,23 +53,25 @@ resource "google_storage_bucket" "data_storage_bucket" {
   location      = "US"
   force_destroy = false
   storage_class = "STANDARD"
-  project       = local.project_name
+  project       = var.project_id
   versioning {
     enabled = true
   }
 }
 
+
+/*
 resource "google_compute_instance" "frontend_container" {
-  name                      = "carboncopy-frontend-terraform-container"
+  name                      = var.frontend_image_name
   description               = "Compute engine for the frontend"
-  zone                      = local.zone
+  zone                      = var.zone
   machine_type              = "e2-micro"
-  project                   = local.project_name
+  project                   = var.project_id
   allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
-      image = "cos-cloud/cos-stable"
+      image = "gcr.io/carbonme/frontend-container-repo/carbon-copy-frontend:latest"
     }
   }
 
@@ -85,4 +83,103 @@ resource "google_compute_instance" "frontend_container" {
     }
   }
 
+  tags = ["http-server", "https-server"]
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    docker pull gcr.io/${var.project_id}/${var.frontend_image_name}:latest
+    docker run -d -p 80:80 gcr.io/${var.project_id}/${var.frontend_image_name}:latest
+  EOF
+
+}
+*/
+
+# This configuration creates a kubernetes cluster on GCP
+# named "primary" and the autopilot mode ensures that
+# GCP manages all the underlying GKE infrastructure.
+resource "google_container_cluster" "primary" {
+  name     = "cc-frontend-primary"
+  location = "us-west1"
+
+  enable_autopilot = true
+
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+}
+
+provider "kubernetes" {
+  host  = "https://${google_container_cluster.primary.endpoint}"
+  token = data.google_client_config.default.access_token
+
+  client_certificate     = base64decode(google_container_cluster.primary.master_auth[0].client_certificate)
+  client_key             = base64decode(google_container_cluster.primary.master_auth[0].client_key)
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
+
+data "google_client_config" "default" {}
+
+# This file specifies what should be run on the GKE
+# cluster - the docker image, the # of replicas etc.
+resource "kubernetes_deployment" "cc-frontend" {
+  metadata {
+    name = "cc-frontend-deploy"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        App = "cc-frontend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          App = "cc-frontend"
+        }
+      }
+
+      spec {
+        container {
+          image = "${var.artifact_registry}/${var.project_id}/${var.frontend_image_name}:${var.tag}"
+          name  = "cc-frontend"
+
+          port {
+            container_port = 3000
+          }
+        }
+      }
+    }
+  }
+}
+
+# This file exposes the backend as a service to
+# be accessible to the outside world
+resource "kubernetes_service" "cc-frontend" {
+  metadata {
+    name = "cc-frontend"
+  }
+
+  spec {
+    selector = {
+      App = "cc-frontend"
+    }
+
+    type = "LoadBalancer"
+
+    port {
+      port        = 3000 # External traffic comes in on this port
+      target_port = 3000 # Traffic to the pods is routed on this port
+    }
+  }
 }
---
+++ service_account.tf
@@ -0,0 +1,32 @@
+# Create service account for kubernetes
+/*
+resource "google_service_account" "kube-cluster" {
+  # TODO: Replace with unique identifier such as email ?
+  account_id   = "kube-cluster"
+  display_name = "Carbon Copy Kube Admin"
+  project      = var.project_id
+}
+
+
+# Grant roles to the service account
+# Allow to administer GKE clusters
+resource "google_project_iam_member" "cluster_admin" {
+  member  = "serviceAccount:${google_service_account.kube-cluster.email}"
+  project = var.project_id
+  role    = "roles/container.admin"
+}
+
+# Allow to view compute resources
+resource "google_project_iam_member" "compute_viewer" {
+  member  = "serviceAccount:${google_service_account.kube-cluster.email}"
+  project = var.project_id
+  role    = "roles/compute.viewer"
+}
+
+# Allow this service account to act as other service accounts
+resource "google_project_iam_member" "service_account_user" {
+  member  = "serviceAccount:${google_service_account.kube-cluster.email}"
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+}
+*/
